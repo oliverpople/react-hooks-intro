b7443e951c3a09b928eff7d706f38c1716c3a373
oliverpople <oliverpople@gmail.com>
2019-04-04 10:14:02 -0700

    Exmaple click counter using react without hooks.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    count: 0
+  };
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
-    return <div>Hellow World</div>;
+    return (
+      <button onClick={this.incrementCount}>
+        I was clicked {this.state.count} times
+      </button>
+    );
   }
 }
 
